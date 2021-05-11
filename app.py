38ea4d3f7714359c76b54fa8b1bc85121d189ad2
leomath42 <leonardomatheus042@gmail.com>
2021-05-11 19:52:16 -0300

    Estrutura básica do projeto.

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/<path>/')
+def post(path):
+    page = path
+    return render_template('post.html', page=page)
+
 
 if __name__ == '__main__':
     app.run(port=5000)
