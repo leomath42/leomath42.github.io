955244454132ad2b365970b7a2c8591faee5c94a
leomath42 <leonardomatheus042@gmail.com>
2021-06-24 20:47:46 -0300

    pagina inicial

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
 from flask import Flask
 from flask import render_template
+from flask_frozen import Freezer
+from flask_flatpages import FlatPages
+import sys
+
 
 app = Flask(__name__)
-
+app.config.from_pyfile('config.cfg')
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.config['FLASK_DEBUG'] = 1
+pages = FlatPages(app)
+FLATPAGES_EXTENSION = '.md'
 
 @app.route('/')
 def index():
@@ -13,8 +21,15 @@ def index():
 @app.route('/<path>/')
 def post(path):
     page = path
-    return render_template('post.html', page=page)
+    # return pages.get("foo")
+    return pages.get('foo').html
+    # return render_template('post.html', page=page)
 
 
 if __name__ == '__main__':
-    app.run(port=5000)
+    if len(sys.argv) > 1 and sys.argv[1] == "freeze":
+        freezer = Freezer(app)
+        freezer.freeze()
+    else:
+        app.run(port=5000, debug=True)
+
