943b2b5682ffc85c3095c8d96416b97431fc17c0
leomath42 <leonardomatheus042@gmail.com>
2021-06-25 02:03:00 -0300

    handle post, building, test and handle error notfound

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 from flask import Flask
+from flask import url_for
 from flask import render_template
 from flask_frozen import Freezer
 from flask_flatpages import FlatPages
+from werkzeug.exceptions import NotFound
+
 import sys
 
 
@@ -10,24 +13,39 @@ app = Flask(__name__)
 app.config.from_pyfile('config.cfg')
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['FLASK_DEBUG'] = 1
+app.config['FREEZER_RELATIVE_URLS']= False#True
 pages = FlatPages(app)
+
 FLATPAGES_EXTENSION = '.md'
+freezer = Freezer(app)
 
 @app.route('/')
 def index():
     return render_template('index.html', index=pages.get("index"))
 
-
-@app.route('/<path>/')
+@app.route('/<path:path>/')
 def post(path):
     page = path
-    return render_template('post.html', page=pages.get(page))
+    return render_template('post.html', page=pages.get_or_404("post/{0}".format(page)))
 
+@app.errorhandler(NotFound)
+def notFound(e):
+	return render_template('notFound.html')
+
+@freezer.register_generator
+def notFound():
+    yield "/404.html"
+
+@freezer.register_generator
+def post():
+	for page in set(pages):
+		if page.path.startswith('post'):
+			yield {'path' : page.path[5:]}
 
 if __name__ == '__main__':
     if len(sys.argv) > 1 and sys.argv[1] == "freeze":
-        freezer = Freezer(app)
-        freezer.freeze()
+    	freezer.freeze()
+    elif len(sys.argv) > 1 and sys.argv[1] == "test":
+    	freezer.run(debug=True)
     else:
         app.run(port=5000, debug=True)
-
